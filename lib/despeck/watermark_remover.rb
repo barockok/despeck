850e85c9ca8f13e1a80461240eb7a600cf325068
Zidni Mubarock <zidmubarock@gmail.com>
2020-09-10 00:45:57 +0700

    check output image has 4 band before composing on removing watermark with --accurate flag

--- lib/despeck/watermark_remover.rb
+++ lib/despeck/watermark_remover.rb
@@ -47,12 +47,11 @@ module Despeck
         WatermarkMask.new(image).find_masks!
       output_image = __remove_watermark__(watermark)
       return unless output_image
-
       no_watermark = no_watermark.colourspace('b-w').bandjoin(mask.invert)
 
-      output_image
-        .bandjoin(mask)
-        .composite(no_watermark, 'over')
+      output_image = output_image.colourspace("srgb") if output_image.bands < 3
+      output_image = output_image.bandjoin(mask) if output_image.bands == 3
+      output_image.composite(no_watermark, 'over')
     end
 
     def __remove_watermark__(image)
@@ -63,7 +62,7 @@ module Despeck
       output_image = grayscale_algorithm(image, wm_color)
       output_image = increase_contrast(output_image) if add_contrast
       output_image = apply_black_improvement(output_image)
-      output_image = apply_grey_to_black(output_image)
+      output_image = apply_grey_to_black(output_image) if wm_color != 'FF0000'
       output_image
     end
 
